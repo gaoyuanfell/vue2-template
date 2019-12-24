c20f501c2ce4961bc521964061f8d1323d4c42dc
moka <gaoyuanfell@sina.com>
2019-12-24 10:40:53 +0800

    1

---
+++ src/view-design.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+
+// import ViewUI from 'view-design';
+// Vue.use(ViewUI);
+import { Button } from 'view-design'
+import 'view-design/dist/styles/iview.css'
+Vue.component('Button', Button)
